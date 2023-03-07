3aa4efe47d09cb8460b5c05df5ab6bf972069195
Farhabi Helal <farhabi.helal@jp.honda-ri.com>
2023-03-07 14:33:43 +0900

    Added madlib gen.

--- dialogflow_utils/games/madlibs/madlibs_gen.py
+++ dialogflow_utils/games/madlibs/madlibs_gen.py
@@ -27,13 +27,34 @@ class MadlibsGenerator:
     def configure(self, config: dict):
         self.config = config
 
-    def create_madlibs(self, madlib_data: list):
+    def create_madlibs(
+        self,
+        madlib_data: list,
+        parent_intent_name: str = None,
+        language_code: str = None,
+    ):
         for madlib in madlib_data:
             madlib: MadlibDataRow
-            self.create_madlib(madlib)
+            self.create_madlib(
+                madlib=madlib,
+                parent_intent_name=parent_intent_name,
+                language_code=language_code,
+            )
             sleep(1)
 
-    def create_madlib(self, madlib: MadlibDataRow, language_code="en"):
+    def create_madlib(
+        self,
+        madlib: MadlibDataRow,
+        parent_intent_name: str = None,
+        language_code: str = "en",
+    ):
+
+        parent = (
+            self.api.intents["display_name"][parent_intent_name]
+            if parent_intent_name
+            else None
+        )
+
         root_intent_obj = (
             dialogflow_v2.Intent()
             if not self.api.intents["display_name"].get(madlib.title)
@@ -44,19 +65,24 @@ class MadlibsGenerator:
         root_intent_obj.action = f"{root_intent_obj.display_name}-question-1"
         root_intent_obj.priority = 0
 
-        # msg = dialogflow_v2.Intent.Message(
-        #     text=dialogflow_v2.Intent.Message.Text(text=[madlib.questions[0]])
-        # )
-        # root_intent_obj.messages = [msg]
-
-        root_intent_obj: dialogflow_v2.Intent = (
-            self.api.create_intent(intent=root_intent_obj, language_code=language_code)
-            if not self.api.intents["display_name"].get(root_intent_obj.display_name)
-            else self.api.update_intent(
-                intent=root_intent_obj, language_code=language_code
-            )
-        )
         root_intent = Intent(root_intent_obj)
+
+        if parent:
+            self.api.create_child(
+                intent=root_intent, parent=parent, language_code=language_code
+            )
+        else:
+            root_intent_obj: dialogflow_v2.Intent = (
+                self.api.create_intent(
+                    intent=root_intent_obj, language_code=language_code
+                )
+                if not self.api.intents["display_name"].get(
+                    root_intent_obj.display_name
+                )
+                else self.api.update_intent(
+                    intent=root_intent_obj, language_code=language_code
+                )
+            )
 
         parent = root_intent
         for i in range(1, len(madlib.questions) + 1):
@@ -64,19 +90,8 @@ class MadlibsGenerator:
             intent_obj.display_name = f"{root_intent_obj.display_name}-question-{i}"
             intent_obj.events = [intent_obj.display_name]
             intent_obj.parent_followup_intent_name = parent.name
-            # intent_obj.action = (
-            #     f"{root_intent_obj.display_name}-question-{i+1}"
-            #     if i < len(madlib.questions)
-            #     else f"{root_intent_obj.display_name}-madlib"
-            # )
-            if i > 1:
-                training_phrase = dialogflow_v2.Intent.TrainingPhrase()
-                part = dialogflow_v2.Intent.TrainingPhrase.Part()
-                part.text = f"{madlib.parameters[i-2]}".replace("$", "").upper()
-                part.entity_type = "@sys.any"
-                training_phrase.parts = [part]
-                intent_obj.training_phrases = [training_phrase]
 
+            if i > 1:
                 parameter = dialogflow_v2.Intent.Parameter()
                 parameter.display_name = f"madlib_param_{i-1}"
                 parameter.default_value = ""
@@ -86,6 +101,15 @@ class MadlibsGenerator:
                 parameter.value = f"${parameter.display_name}"
 
                 intent_obj.parameters = [parameter]
+
+                training_phrase = dialogflow_v2.Intent.TrainingPhrase()
+                part = dialogflow_v2.Intent.TrainingPhrase.Part()
+                part.text = f"{madlib.parameters[i-2]}".replace("$", "").upper()
+                part.entity_type = "@sys.any"
+                part.user_defined = True
+                part.alias = parameter.display_name
+                training_phrase.parts = [part]
+                intent_obj.training_phrases = [training_phrase]
 
             msg = dialogflow_v2.Intent.Message(
                 text=dialogflow_v2.Intent.Message.Text(text=[madlib.questions[i - 1]])
@@ -106,13 +130,6 @@ class MadlibsGenerator:
         response_intent_obj.events = [response_intent_obj.display_name]
         response_intent_obj.action = f"{root_intent_obj.display_name}-madlib"
 
-        training_phrase = dialogflow_v2.Intent.TrainingPhrase()
-        part = dialogflow_v2.Intent.TrainingPhrase.Part()
-        part.text = f"{madlib.parameters[-1]}".replace("$", "").upper()
-        part.entity_type = "@sys.any"
-        training_phrase.parts = [part]
-        response_intent_obj.training_phrases = [training_phrase]
-
         parameter = dialogflow_v2.Intent.Parameter()
         parameter.display_name = f"madlib_param_{len(madlib.parameters)}"
         parameter.default_value = ""
@@ -122,6 +139,34 @@ class MadlibsGenerator:
         parameter.value = f"${parameter.display_name}"
 
         response_intent_obj.parameters = [parameter]
+
+        training_phrase = dialogflow_v2.Intent.TrainingPhrase()
+        part = dialogflow_v2.Intent.TrainingPhrase.Part()
+        part.text = f"{madlib.parameters[-1]}".replace("$", "").upper()
+        part.entity_type = "@sys.any"
+        part.user_defined = True
+        part.alias = parameter.display_name
+        training_phrase.parts = [part]
+        response_intent_obj.training_phrases = [training_phrase]
+
+        responses = [
+            "Ready?",
+            "Here's your madlib.",
+            "This is good.",
+            "Perfect.",
+            "Here we go.",
+            "This is pretty good.",
+            "This one's cute.",
+            "Okay, listen to this.",
+            "I like this.",
+            "Oh! This is perfect.",
+            "Listen to this.",
+        ]
+
+        msg = dialogflow_v2.Intent.Message(
+            text=dialogflow_v2.Intent.Message.Text(text=responses)
+        )
+        response_intent_obj.messages = [msg]
 
         response_intent = Intent(response_intent_obj)
         self.api.create_child(
@@ -155,13 +200,25 @@ class MadlibsGenerator:
             intent=madlib_intent, parent=response_intent, language_code=language_code
         )
 
-    def run(self, db_path: str = None, question_count: int = None):
+    def run(
+        self,
+        db_path: str = None,
+        parent_intent_name: str = None,
+        question_count: int = None,
+        language_code: str = None,
+    ):
         madlib_data: list = self.parser.run(
             db_path=db_path, question_count=question_count
         )
-        # print(madlib_data)
-
-        self.create_madlibs(madlib_data)
+        self.create_madlibs(
+            madlib_data=madlib_data,
+            parent_intent_name=parent_intent_name
+            if parent_intent_name
+            else self.config["parent_intent_name"],
+            language_code=language_code
+            if language_code
+            else self.config["language_code"],
+        )
 
 
 if __name__ == "__main__":
@@ -173,6 +230,8 @@ if __name__ == "__main__":
         "db_path": os.path.join(data_dir, "Madlibs Scripts.xlsx"),
         "credential": os.path.join(keys_dir, "haru-chat-games.json"),
         "question_count": 3,
+        "parent_intent_name": "madlibs",
+        "language_code": "en",
     }
     gen = MadlibsGenerator(config)
     gen.run()
