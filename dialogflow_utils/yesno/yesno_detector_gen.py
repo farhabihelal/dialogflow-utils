3f10cf7922102cf104ddcabaa4397fc08f9904a4
Farhabi Helal <farhabi.helal@jp.honda-ri.com>
2023-03-29 19:23:29 +0900

    Not creating YNF nodes if exist in YN gen.

--- dialogflow_utils/yesno/yesno_detector_gen.py
+++ dialogflow_utils/yesno/yesno_detector_gen.py
@@ -41,42 +41,50 @@ class YesNoDetectorGenerator:
             )
 
         # yes node
-        yes_intent_obj = dialogflow_v2.Intent()
-        yes_intent_obj.display_name = f"{parent.display_name}-yes"
-        yes_intent_obj.events = [yes_intent_obj.display_name]
-        yes_intent_obj.messages = [
-            dialogflow_v2.Intent.Message(
-                payload={
-                    "node_type": "AnswerNode",
-                    "local_classifier_class": "Yes",
-                }
-            ),
-            dialogflow_v2.Intent.Message(
-                text=dialogflow_v2.Intent.Message.Text(
-                    text=["this is a yes response.".title()]
-                )
-            ),
-        ]
-        yes_intent = Intent(yes_intent_obj)
+        yes_intent: Intent = self.api.intents["display_name"].get(
+            f"{parent.display_name}-yes"
+        )
+        if not yes_intent:
+            yes_intent_obj = dialogflow_v2.Intent()
+            yes_intent_obj.display_name = f"{parent.display_name}-yes"
+            yes_intent_obj.events = [yes_intent_obj.display_name]
+            yes_intent_obj.messages = [
+                dialogflow_v2.Intent.Message(
+                    payload={
+                        "node_type": "AnswerNode",
+                        "local_classifier_class": "Yes",
+                    }
+                ),
+                dialogflow_v2.Intent.Message(
+                    text=dialogflow_v2.Intent.Message.Text(
+                        text=["this is a yes response.".title()]
+                    )
+                ),
+            ]
+            yes_intent = Intent(yes_intent_obj)
 
         # no node
-        no_intent_obj = dialogflow_v2.Intent()
-        no_intent_obj.display_name = f"{parent.display_name}-no"
-        no_intent_obj.events = [no_intent_obj.display_name]
-        no_intent_obj.messages = [
-            dialogflow_v2.Intent.Message(
-                payload={
-                    "node_type": "AnswerNode",
-                    "local_classifier_class": "No",
-                }
-            ),
-            dialogflow_v2.Intent.Message(
-                text=dialogflow_v2.Intent.Message.Text(
-                    text=["this is a no response.".title()]
-                )
-            ),
-        ]
-        no_intent = Intent(no_intent_obj)
+        no_intent: Intent = self.api.intents["display_name"].get(
+            f"{parent.display_name}-no"
+        )
+        if not no_intent:
+            no_intent_obj = dialogflow_v2.Intent()
+            no_intent_obj.display_name = f"{parent.display_name}-no"
+            no_intent_obj.events = [no_intent_obj.display_name]
+            no_intent_obj.messages = [
+                dialogflow_v2.Intent.Message(
+                    payload={
+                        "node_type": "AnswerNode",
+                        "local_classifier_class": "No",
+                    }
+                ),
+                dialogflow_v2.Intent.Message(
+                    text=dialogflow_v2.Intent.Message.Text(
+                        text=["this is a no response.".title()]
+                    )
+                ),
+            ]
+            no_intent = Intent(no_intent_obj)
 
         # fallback node
         if not fallback_intent:
@@ -127,7 +135,10 @@ class YesNoDetectorGenerator:
             # check parent node type
             parent_node_type: str = parent.custom_payload.get("node_type")
 
-            if parent_node_type and parent_node_type != "QuestionNode":
+            if parent_node_type and parent_node_type not in [
+                "QuestionNode",
+                "AnswerQuestionNode",
+            ]:
                 raise ValueError(
                     f"`{parent.display_name}` has invalid node type. Expecting type `QuestionNode` but found `{parent_node_type}`"
                 )
@@ -150,13 +161,29 @@ class YesNoDetectorGenerator:
 
 if __name__ == "__main__":
 
-    intent_names = ["express-corolla"]
+    intent_names = [
+        # pet
+        # "topic-pet-cat-followup",
+        # "topic-pet-bird-followup",
+        "topic-pet-hypothetical-pet-refresh-harder-cat",
+        "topic-pet-hypothetical-pet-refresh-harder-dog",
+        # lemurs
+        "topic-lemurs-destination-merge",
+        "topic-lemurs-are-from",
+        "topic-lemurs-pet-collected",
+        "topic-lemurs-fav-animal-collected",
+        "topic-lemurs-no-animal-collected",
+        "topic-lemurs-pet-collected-home-country-collected",
+        "topic-lemurs-pet-collected-home-country-not-collected",
+        "topic-lemurs-user-would-mind-no",
+    ]
 
     base_dir = os.path.abspath(f"{os.path.dirname(__file__)}/../..")
     keys_dir = os.path.join(base_dir, ".temp/keys")
 
     config = {
-        "credential": os.path.join(keys_dir, "haru-test.json"),
+        "credential": os.path.join(keys_dir, "es.json"),
+        # "credential": os.path.join(keys_dir, "haru-test.json"),
         "intent_names": intent_names,
         "language_code": "en",
     }
