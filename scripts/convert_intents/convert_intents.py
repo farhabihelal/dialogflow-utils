505809c9c8eab64e67a28219206036ceb752aa86
Farhabi Helal <farhabi.helal@jp.honda-ri.com>
2023-03-29 17:19:11 +0900

    added payload mod in intent conversion.

--- scripts/convert_intents/convert_intents.py
+++ scripts/convert_intents/convert_intents.py
@@ -12,17 +12,24 @@ if __name__ == "__main__":
         )
     )
 
-    from dialogflow import Dialogflow
+    from dialogflow import Dialogflow, Intent
 
     from time import sleep
 
     intent_names = [
-        "basketball-fact-yes",
-        "baseball-fact-yes",
-        "tennis-fact-yes",
-        "knows-basketball-fact-no",
-        "knew-baseball-fact-no",
-        "knew-tennis-fact-no",
+        # "basketball-fact-yes",
+        # "baseball-fact-yes",
+        # "tennis-fact-yes",
+        # "knows-basketball-fact-no",
+        # "knew-baseball-fact-no",
+        # "knew-tennis-fact-no",
+        "like-sports",
+        # "other-sports",
+        # "likes-to-watch-sports-or-fallback",
+        # "watch-in-person",
+        # "watches-on-tv - fallback",
+        # "play-for-fun-fallback",
+        # "coach-reaction",
     ]
 
     base_dir = os.path.abspath(f"{os.path.dirname(__file__)}/../../")
@@ -38,13 +45,15 @@ if __name__ == "__main__":
 
     for i, intent_name in enumerate(intent_names):
         intent_name: str
-        intent_obj = df.intents["display_name"].get(intent_name)
-        if not intent_obj:
+        intent: Intent = df.intents["display_name"].get(intent_name)
+        if not intent:
             print(f"Error: {intent_name} not found!")
             continue
-        df.convert_intent_type(
-            intent=intent_obj, language_code="en", make_fallback=False
-        )
+
+        payload = intent.custom_payload
+        payload["node_type"] = "QuestionNode"
+        intent.custom_payload = payload
+        df.convert_intent_type(intent=intent, language_code="en", make_fallback=False)
 
         print(f"Success: {intent_name}")
         if i + 1 < len(intent_names):
