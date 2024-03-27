768a282e3114976878f6842d480c3e62b67a15ee
Farhabi Helal <farhabi.helal@jp.honda-ri.com>
2024-03-27 14:43:05 +0900

    Added fallback gen.

---
+++ dialogflow_utils/sentiment/fallback_sentiment_gen.py
@@ -0,0 +1,140 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(f"{os.path.dirname(__file__)}/.."))
+
+from dialogflow_api.src.dialogflow import Dialogflow, Intent
+
+import google.cloud.dialogflow_v2 as dialogflow_v2
+
+from time import sleep
+
+
+class FallbackSentimentGenerator:
+    def __init__(self, config: dict) -> None:
+        self.configure(config)
+
+        self.api = self.config.get("api")
+        if not self.api:
+            self.api = Dialogflow(config)
+            self.api.get_intents()
+            self.api.generate_tree()
+
+        self.valid_sentiments = ["positive", "negative", "neutral"]
+
+    def configure(self, config: dict):
+        self.config = config
+
+    def add_metadata(self, parent: Intent, sentiment_intents: dict):
+        payload = parent.custom_payload
+        metadata = {
+            "sentiment_classification_override": {
+                "positive_intent": f"{sentiment_intents['positive'].display_name}",
+                "neutral_limit": 0.5,
+                "neutral_intent": f"{sentiment_intents['neutral'].display_name}",
+                "negative_limit": -0.5,
+                "negative_intent": f"{sentiment_intents['negative'].display_name}",
+            },
+        }
+        payload.update(metadata)
+        parent.custom_payload = payload
+
+        self.api.update_intent(
+            intent=parent.intent_obj, language_code=self.config["language_code"]
+        )
+
+    def get_sentiment_intents(self, parent: Intent) -> dict:
+        return {x: self.get_sentiment_intent(x, parent) for x in self.valid_sentiments}
+
+    def get_sentiment_intent(self, sentiment: str, parent: Intent) -> Intent:
+        sentiment = sentiment.lower()
+
+        if sentiment not in self.valid_sentiments:
+            raise ValueError(f"Invalid sentiment value: {sentiment}")
+
+        intent_obj = dialogflow_v2.Intent()
+        intent_obj.display_name = f"{parent.display_name}-{sentiment}"
+        intent_obj.events = [intent_obj.display_name]
+        intent_obj.action = self.get_action(parent)
+        intent_obj.messages.append(
+            dialogflow_v2.Intent.Message(payload={"node_type": "RepeatNode"})
+        )
+        intent_obj.messages.append(
+            dialogflow_v2.Intent.Message(
+                text=dialogflow_v2.Intent.Message.Text(
+                    text=[f"this is {sentiment} sentiment path.".title()]
+                )
+            )
+        )
+        intent = Intent(intent_obj)
+        return intent
+
+    def get_action(self, parent: Intent):
+        action = ""
+
+        if parent.intent_obj.action:
+            action = parent.intent_obj.action
+
+        elif len(parent.children) == 1:
+            action = parent.children[0].intent_obj.action
+
+        elif len(parent.children) > 1:
+            for x in parent.children:
+                x: Intent
+                if x.intent_obj.is_fallback:
+                    action = x.intent_obj.action
+
+        return action
+
+    def run(self, intent_names=None, language_code="en", refresh=True):
+        if refresh:
+            self.api.get_intents()
+            self.api.generate_tree()
+
+        parent_names = intent_names if intent_names else self.config["intent_names"]
+
+        for parent_name in parent_names:
+            parent_name: str
+            parent: Intent = self.api.intents["display_name"].get(parent_name)
+            if not parent:
+                print(f"Parent intent `{parent_name}` not found.")
+                continue
+            sentiment_intents: dict = self.get_sentiment_intents(parent)
+
+            self.add_metadata(parent, sentiment_intents)
+
+            self.api.create_children(
+                intents=list(sentiment_intents.values()),
+                parent=parent,
+                language_code=language_code,
+            )
+
+            # remove action from parent
+            parent.intent_obj.action = ""
+            self.api.update_intent(
+                intent=parent.intent_obj, language_code=language_code
+            )
+            sleep(2)
+
+
+if __name__ == "__main__":
+
+    intent_names = [
+        # "topic-hometown-what-answer",
+        # "topic-hometown-big-city",
+        # "topic-hometown-unknown-city-size",
+        "topic-travel-homecountry-human-seen-the-northern-lights",
+    ]
+
+    base_dir = os.path.abspath(f"{os.path.dirname(__file__)}/../..")
+    keys_dir = os.path.join(base_dir, ".temp/keys")
+
+    config = {
+        "api": None,
+        "credential": os.path.join(keys_dir, "es.json"),
+        "intent_names": intent_names,
+        "language_code": "en",
+    }
+
+    gen = FallbackSentimentGenerator(config)
+    gen.run()
---
+++ dialogflow_utils/sentiment/fallback_sentiment_gen_unsafe.py
@@ -0,0 +1,154 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(f"{os.path.dirname(__file__)}/.."))
+
+from dialogflow_api.src.dialogflow import Dialogflow, Intent
+
+import google.cloud.dialogflow_v2 as dialogflow_v2
+
+from time import sleep
+
+from sentiment.sentiment_gen import SentimentGenerator
+
+
+class SentimentGeneratorUnsafe(SentimentGenerator):
+    def __init__(self, config: dict) -> None:
+        super().__init__(config)
+
+    def get_action(self, parent: Intent):
+        action = ""
+
+        if parent.intent_obj.action:
+            action = parent.intent_obj.action
+
+        elif len(parent.children) == 1:
+            action = parent.children[0].intent_obj.action
+
+        elif len(parent.children) > 1:
+            for x in parent.children:
+                x: Intent
+                if x.intent_obj.is_fallback:
+                    action = x.intent_obj.action
+
+        return action
+
+    def create_dummy(self, parent: Intent, language_code: str = None) -> Intent:
+        dummy_intent_obj = dialogflow_v2.Intent()
+        dummy_intent_obj.display_name = f"{parent.display_name}-dummy"
+        dummy_intent_obj.events = [dummy_intent_obj.display_name]
+        # dummy_intent_obj.priority = -1
+
+        dummy_intent_obj.messages = [
+            dialogflow_v2.Intent.Message(payload={"node_type": "DisabledNode"})
+        ]
+
+        dummy_intent = Intent(dummy_intent_obj)
+
+        parent.intent_obj.action = dummy_intent_obj.display_name
+        dummy_intent = self.api.create_child(
+            intent=dummy_intent, parent=parent, language_code=language_code
+        )
+        return dummy_intent
+
+    def run(self, intent_names=None, language_code="en", refresh=True):
+        if refresh:
+            self.api.get_intents()
+            self.api.generate_tree()
+
+        parent_names = intent_names if intent_names else self.config["intent_names"]
+
+        for i, parent_name in enumerate(parent_names):
+            parent_name: str
+            parent: Intent = self.api.intents["display_name"].get(parent_name)
+            if not parent:
+                print(f"Parent intent `{parent_name}` not found.")
+                continue
+
+            payload: dict = parent.custom_payload
+            payload.update({"sentiment_classification_override": {}})
+            parent.custom_payload = payload
+
+            # update parent
+            # parent._intent_obj = self.api.update_intent(
+            #     intent=parent, language_code=language_code
+            # )
+
+            dummy_intent: Intent = self.create_dummy(parent)
+            sentiment_intents: dict = self.get_sentiment_intents(dummy_intent)
+            self.add_metadata(dummy_intent, sentiment_intents)
+
+            self.api.create_children(
+                intents=list(sentiment_intents.values()),
+                parent=dummy_intent,
+                language_code=language_code,
+            )
+
+            print(f"{parent_name}: success")
+
+            if i + 1 < len(parent_names):
+                sleep(10)
+
+
+if __name__ == "__main__":
+
+    intent_names = [
+        # intro
+        # "topic-intro-engage-smalltalk-must-fallback",
+        # "topic-intro-name-double-check-fallback",
+        # age
+        # "topic-day-one-session-one-age-again-fallback",
+        # "topic-day-one-session-one-age-fallback",
+        # "topic-day-one-session-one-age-parents-fallback"
+        # "topic-day-one-session-one-age-grandparents-fallback",
+        # "topic-day-one-session-one-age-parents-job-sarcastic-handle-fallback",
+        # names origins
+        # "topic-day-one-session-one-names-origins-meaning-fallback",
+        # "topic-day-one-session-one-names-origins-meaning-haru-fallback",
+        # "topic-day-one-session-one-names-happy-with-name-fallback",
+        # "topic-day-one-session-one-remembering-names-fallback",
+        # "topic-day-one-session-one-haru-name-fallback",
+        # "topic-day-one-session-one-last-name-fallback",
+        # "topic-day-one-session-one-confirm-last-name-fallback",
+        # "topic-day-one-session-one-family-name-capture-again-fallback",
+        # "topic-day-one-session-one-family-name-double-check-fallback",
+        # "topic-day-one-session-one-collect-name-origin-fallback",
+        # "topic-day-one-session-one-haru-origin-fallback",
+        # "topic-day-one-session-one-ever-been-fallback",
+        # "topic-day-one-session-one-collect-name-origin-sarcastic-handle-fallback",
+        # "topic-day-one-session-one-collect-name-origin-confirm-fallback",
+        # "topic-day-one-session-one-haru-origin-sarcastic-handle-fallback",
+        # hometown
+        # "topic-hometown-fallback",
+        # "topic-hometown-still-live-there-fallback",
+        # "topic-hometown-still-live-there-no-fallback",
+        # "topic-hometown-unknown-type-of-building",
+        # "topic-hometown-looking-for-a-new-roommate-fallback",
+        # "topic-hometown-new-roommate-yes-reaction",
+        # "topic-hometown-looking-for-a-new-roommate-no-reaction",
+        "topic-hometown-not-from-homecountry-capture-birthcountry",
+        "topic-hometown-homecountry-live-now-fallback",
+        "topic-hometown-what-question-fallback",
+        # travel homecountry
+        # "topic-travel-homecountry-human-guesses-harus-from-non-japan-country",
+        # "topic-travel-homecountry-human-from-other-country",
+        # "topic-travel-homecountry-human-unsure-about-haru-visiting",
+        # "topic-travel-homecountry-favorite-hemisphere-any-answer",
+        # "topic-travel-homecountry-human-been-to-sweden-fallback",
+        # "topic-travel-homecountry-human-seen-the-northern-lights-fallback",
+        # "topic-travel-homecountry-wants-to-know-where-the-lights-come-from",
+        # "topic-travel-homecountry-loves-how-humans-interact-with-nature",
+    ]
+
+    base_dir = os.path.abspath(f"{os.path.dirname(__file__)}/../..")
+    keys_dir = os.path.join(base_dir, ".temp/keys")
+
+    config = {
+        "api": None,
+        "credential": os.path.join(keys_dir, "es.json"),
+        "intent_names": intent_names,
+        "language_code": "en",
+    }
+
+    gen = SentimentGeneratorUnsafe(config)
+    gen.run()
